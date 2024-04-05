9c05ff7ae826a2296a1024e90bdc23714c2c54e7
LRWyatt801 <loganwyatt1995@gmail.com>
2024-04-05 10:47:30 -0500

    Opfinder function writen not tested

--- ophandler.c
+++ ophandler.c
@@ -1,6 +1,5 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 /**
  * opfinder - finds the operation/function to run
@@ -9,7 +8,26 @@
  * Return: function pointer
 */
 
-void (*opfinder())(stack_t **, unsigned int)
+void (*opfinder(char **op_arr))(stack_t **, unsigned int)
 {
-	
+	instruction_t ops[] = {
+		{"push", stk_push},
+		{"pall", stk_pall},
+		/*{"pint", stk_pint},
+		{"pop", stk_pop},
+		{"swap", stk_swap},
+		{"add", stk_add},
+		{"nop", stk_nop},*/
+		{NULL, NULL}
+	};
+	int i;
+
+	while (ops[i].opcode != NULL)
+	{
+		if (strcmp(ops[i].opcode, op_arr[0]) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_count, op_arr[0]);
+	exit(EXIT_FAILURE);
 }
