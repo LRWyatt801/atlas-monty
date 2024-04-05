8444a6268aafeb8329b0cdad108a3c12ed22bfcd
LRWyatt801 <loganwyatt1995@gmail.com>
2024-04-04 19:24:59 -0500

    Set up README.md, monty.h, and main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ * main - entry point for program "monty"
+ * @argc: number of argv
+ * @argv: argument input, in this case a file
+ *
+ * Return: EXIT_SUCCESS on a success, or EXIT_FAILURE for any error
+*/
+
+int main(int argc, char **argv)
+{
+	stack_t *montystk = NULL;
+	int count = 1; /*line count starts at 1 for 1st line*/
+	
+}
